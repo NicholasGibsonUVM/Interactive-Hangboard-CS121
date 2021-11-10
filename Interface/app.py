9501dea5efcad57aa8228db9e322483648ee3577
NicholasGibsonUVM <Nicholas.Gibson@uvm.edu>
2021-11-10 12:29:55 -0500

    added index.php

--- Interface/app.py
+++ Interface/app.py
@@ -4,6 +4,10 @@ import json
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def frontPage():
+    return render_template("index.php")
+    
 @app.route('/login', methods=['GET'])
 def login():
     return render_template("login.php")
