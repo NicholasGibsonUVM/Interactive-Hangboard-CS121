5c20e27a92569e470a53c98bdc6a25bff05539b0
NicholasGibsonUVM <nsgibson@uvm.edu>
2021-12-04 10:43:06 -0500

    added bar chart

--- Interface/static/chartUpdate.js
+++ Interface/static/chartUpdate.js
@@ -2,4 +2,8 @@ function chartUpdate(id) {
     myChart.data.datasets[0].data = timeY[id];
     myChart.data.labels = repsX[id]
     myChart.update();
+
+    barChart.data.datasets[0].data = barData[id];
+    barChart.data.labels = ['Hold 1', 'Hold 2', 'Hold 3']
+    barChart.update();
 }
--- Interface/templates/charts.php
+++ Interface/templates/charts.php
@@ -3,6 +3,7 @@ $sqlSessions = 'SELECT `pmkSessionId` FROM `tblSession` WHERE `fpkUsername` = "'
 $sessionList = $databaseWriter->select($sqlSessions);
 $hangs = array();
 $reps = array();
+$holds = array();
 $counter = 0;
 foreach ($sessionList as $sessionId) {
     $sqlSession = 'SELECT `fldTime`, `fldHold` FROM `tblHang` WHERE `fpkSessionId` = ' . $sessionId['pmkSessionId'];
@@ -14,6 +15,7 @@ foreach ($sessionList as $sessionId) {
     foreach ($oneSession as $hang) {
         $hangs[$counter][$repCounter] = $hang['fldTime'];
         $reps[$counter][$repCounter] = $repCounter + 1;
+        $holds[$counter][$repCounter] = $hang['fldHold'];
         $repCounter++;
     }
     $counter++;
@@ -23,17 +25,60 @@ foreach ($sessionList as $sessionId) {
 <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.4/Chart.js"></script>
 <section class="chartBox">
     <canvas id="myChart"></canvas>
+    <canvas id="barChart"></canvas>
 </section>
 <script>
     //setup block
-    const timeY = <?php echo json_encode($hangs); ?> // goes in data
-    const repsX = <?php echo json_encode($reps); ?> //goes in labels
+    const timeY = <?php echo json_encode($hangs); ?>; // goes in data
+    const repsX = <?php echo json_encode($reps); ?>; //goes in labels
+    const holdAmounts = <?php echo json_encode($holds); ?>;
+    var barData = [];
+    for (let i = 0; i < Object.keys(holdAmounts).length; i++) {
+        let hold1 = 0
+        let hold2 = 0
+        let hold3 = 0
+        for (let j = 0; j < holdAmounts[i].length; j++) {
+            if (holdAmounts[i][j] == 1) {
+                hold1++;
+            } else if (holdAmounts[i][j] == 2) {
+                hold2++;
+            } else if (holdAmounts[i][j] == 3) {
+                hold3++;
+            }
+        }
+        barData.push([hold1,hold2,hold3])
+    }
     //data block
     const data = {
         labels: repsX[0],
         datasets: [{
             label: 'reps',
             data: timeY[0],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    }
+
+    const dataBar = {
+        labels: ['Hold 1', 'Hold 2', 'Hold 3'],
+        datasets: [{
+            label: 'Hold',
+            data: barData[0],
             backgroundColor: [
                 'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
@@ -62,12 +107,40 @@ foreach ($sessionList as $sessionId) {
                 y: {
                     beginAtZero: true
                 }
+            },
+            title: {
+                display: true,
+                text: 'Timg Hanging Vs. Reps'
             }
         }
-    }
+    };
+
+    const configBar = {
+        type: 'bar',
+        data,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                },
+                x: {
+                    suggestedMax: 3
+                }
+            },
+            title: {
+                display: true,
+                text: 'Reps Vs. Hold Type'
+            }
+        }
+    };
     //render block
     const myChart = new Chart(
         document.getElementById('myChart'),
         config,
+    );
+
+    const barChart = new Chart(
+        document.getElementById('barChart'),
+        configBar,
     );
 </script>
