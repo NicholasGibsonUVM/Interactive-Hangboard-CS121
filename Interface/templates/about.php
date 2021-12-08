5b9d75b3959cf4d063ea912b3511358fc89d49c9
NicholasGibsonUVM <nsgibson@uvm.edu>
2021-12-08 09:30:41 -0500

    added picture

--- Interface/templates/about.php
+++ Interface/templates/about.php
@@ -14,7 +14,7 @@ include 'top.php';
             <p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
         <div class='about'>
-            <img class = "imgAbout" href = "../images/nick.png">
+            <img class = "imgAbout" href = "../images/nick.jpg">
             <h3>Nicholas Gibson</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
         </div>
--- Interface/templates/charts.php
+++ Interface/templates/charts.php
@@ -72,17 +72,17 @@ foreach ($sessionList as $sessionId) {
             label: 'Hold',
             data: barData[0],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
+                'rgba(144, 238, 144, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
                 'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
+                'rgba(144, 238, 144, 1)',
+                'rgba(255, 99, 132, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
                 'rgba(255, 159, 64, 1)'
@@ -97,7 +97,9 @@ foreach ($sessionList as $sessionId) {
         options: {
             scales: {
                 yAxes: {
-                    beginAtZero: true
+                    ticks: {
+                        beginAtZero: true
+                    }
                 },
                 xAxes: {
                     type: 'time'
@@ -116,7 +118,9 @@ foreach ($sessionList as $sessionId) {
         options: {
             scales: {
                 yAxes: {
-                    beginAtZero: true,
+                    ticks: {
+                        beginAtZero: true
+                    }
                 },
                 xAxes: {
                     suggestedMax: 3,
